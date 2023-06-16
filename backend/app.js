a293b40eadac3a86818f8c849cf2b28fffb58999
BuildTools <schodllukas8@gmail.com>
2023-06-16 12:20:13 +0200

    connection to websocket server working

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const ws = require('ws');
 const app = express();
 const { createTables, connectToDatabase } = require('./database');
 
@@ -20,6 +21,18 @@ app.use((err, req, res, next) => {
     });
 });
 
-app.listen(4111, () => console.log("Server has started at port 4111"))
+const wsServer = new ws.Server({ noServer: true });
+wsServer.on('connection', socket => {
+    socket.on('message', message => console.log(message));
+});
+
+const server = app.listen(4111, () => console.log("Server has started at port 4111"))
+
+// WebSocket server runs on the same port as the express server
+server.on('upgrade', (request, socket, head) => {
+    wsServer.handleUpgrade(request, socket, head, socket => {
+        wsServer.emit('connection', socket, request);
+    });
+});
 
 module.exports = db;
